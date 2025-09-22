3b4f2a79b1ba69db914253a81ab8f2a6ac83919b
Alex Pickering <alexvpickering@gmail.com>
2025-09-22 10:57:32 -0400

    cleanup sarahs work2

--- scripts/merge_microbe.R
+++ scripts/merge_microbe.R
@@ -9,41 +9,17 @@ microbe_sheets <- excel_sheets(microbe_file)
 microbe <- lapply(microbe_sheets, function(sheet) read_excel(microbe_file, sheet = sheet))
 names(microbe) <- microbe_sheets
 
-# used by process_database to split into data.frame per study
+# used to split into data.frame per study
 split_by_study <- function(db) {
-  study_nums <- db$Number
-  study_nums_unique <- na.omit(unique(study_nums))
   
-  res <- list()
-  
-  for (study_num in study_nums_unique) {
-    
-    # first row for study
-    start <- which(study_nums == study_num)
-    stopifnot(length(start) == 1)
-    
-    # how far til next study
-    jump <- which(!is.na(
-      study_nums[(start+1):length(study_nums)]
-      ))[1]
-    
-    # edge case: last study num
-    if (is.na(jump)) jump <- length(study_nums)
-    
-    # subset to study and drop Number column
-    db_study <- db[start:(start+jump-1), -1]
-    
-    # drop all NA rows
-    db_study <- db_study[!apply(is.na(db_study), 1, all), ]
-    
-    # store in list
-    res[[study_num]] <- db_study
-  }
-  
-  return(res)
+  db |>
+    tidyr::fill(Number, .direction = "down") |> 
+    dplyr::group_split(Number, .keep = FALSE) |> 
+    setNames(unique(na.omit(db$Number))) |> 
+    map(~ dplyr::filter(.x, !if_all(dplyr::everything(), is.na)))
 }
-
-# for processing "Old DATABASE" and "New DATABASE"
+  
+# for processing "Old DATABASE", "New DATABASE", and "Sarah's Work"
 process_database <- function(db) {
   
   # remove reference column and empty column
@@ -53,7 +29,7 @@ process_database <- function(db) {
   elevated_in_health_cols <- 2:9
   elevated_in_perio_cols <- 10:17
   
-  # split into up/down regulated
+  # split into up/down regulated, keeping study number
   db_up <- db[, c(1, elevated_in_perio_cols)]
   db_dn <- db[, c(1, elevated_in_health_cols)]
   
@@ -74,21 +50,60 @@ process_database <- function(db) {
   ))
 }
 
+# NOTE: sarahs_db2 has taxonomy ids (yay!)
+# for processing "Sarah's Work (2)"
+process_sarahs_db2 <- function(db) {
+  db$...15 <- db$Reference <- NULL
+  
+  # indices for columns that are elevated in health/perio
+  elevated_in_health_cols <- 2:13
+  elevated_in_perio_cols <- 14:25
+  
+  # drop first row (header info e.g. "elevated in ...")
+  db <- db[-1, ]
+  
+  # split into up/down regulated, keeping study number
+  db_up <- db[, c(1, elevated_in_perio_cols)]
+  db_dn <- db[, c(1, elevated_in_health_cols)]
+  
+  # clean up column names
+  colnames(db_up) <- gsub('[.]{3}\\d+$', '', colnames(db_up))
+  colnames(db_dn) <- gsub('[.]{3}\\d+$', '', colnames(db_dn))
+  
+  # split by study number
+  db_up <- split_by_study(db_up)
+  db_dn <- split_by_study(db_dn)
+  
+  return(list(
+    db_up = db_up,
+    db_dn = db_dn
+  ))
+  
+}
+
 new_database <- process_database(microbe$`New DATABASE`)
 old_database <- process_database(microbe$`Old DATABASE`)
+sarahs_db <- process_database(microbe$`Sarah's Work`)
+sarahs_db2 <- process_sarahs_db2(microbe$`Sarah's Work (2`)
 
 # check study overlap with overview data.frame
 overview_file <- 'output/overview_merged.rds'
 overview <- readRDS(overview_file)
 
-# none of New DATABASE is present in overview studies
-# all of Old Database is present in overview studies
+# none of "New DATABASE" is present in overview studies
 table(names(new_database$db_up) %in% overview$Number)
+
+# all of "Old Database" is present in overview studies
 table(names(old_database$db_up) %in% overview$Number)
 
-# NOTE: sarahs_db2 has taxonomy ids (yay!)
-sarahs_db <- microbe$`Sarah's Work`
-sarahs_db2 <- microbe$`Sarah's Work (2`
+# one study in "Sarah's Work" is duplicated in overview studies (462 adjusted vs unadjusted?)
+names(sarahs_db$db_up)[!names(sarahs_db$db_up) %in% overview$Number]
+'462' %in% overview$Number
 
+# all of "Sarah's Work (2)" is present in overview studies
+table(names(sarahs_db2$db_up) %in% overview$Number)
+
+# all of "Sarah's Work" is present in "Sarah's Work (2)"
+table(names(sarahs_db$db_up) %in% names(sarahs_db2$db_up))
 
 
